94fff93859edcc3a200039fb3d323958fed01a63
Sh4d0wJ0J0 <jojogamerbestyeya@gmail.com>
2024-11-04 20:18:06 +0100

    IN-DEV

--- app/api/v1/characters/[id]/route.ts
+++ app/api/v1/characters/[id]/route.ts
@@ -1,14 +1,19 @@
 import { Character } from '@/app/types';
 import { characters } from '@/app/data/characters';
 
+interface PageProps {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  pageProps: PageProps
 ) {
   try {
     const { searchParams } = new URL(request.url);
     const fields = searchParams.get('fields')?.split(',').map(field => decodeURIComponent(field));
-    const id = parseInt(params.id);
+    const id = parseInt(pageProps.params.id);
     
     const character = characters.find(char => char.id === id);
     
